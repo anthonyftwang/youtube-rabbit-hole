1c3da9a923b9482bb25183e92e0865a5d388bec8
Anthony Wang <52114040+anthonyftwang@users.noreply.github.com>
2022-01-16 15:34:49 -0500

    Update code style

--- js/nav.js
+++ js/nav.js
@@ -1,54 +1,65 @@
-chrome.storage.sync.get("hideNav", function(result) {
-    var hideNav = result.hideNav;
-    if (hideNav == true) {
-        window.setInterval(function() {
-            hideNavDrawer();
-        }, 100);
-    }
+chrome.storage.sync.get("hideNav", function (result) {
+  var hideNav = result.hideNav;
+  if (hideNav == true) {
+    window.setInterval(function () {
+      hideNavDrawer();
+    }, 100);
+  }
 });
 
 function hideNavDrawer() {
-    /* ~~ buggy implementation ~~
-    var nav = document.querySelector("ytd-guide-renderer");
-    if (nav) {
-        nav.innerHTML = "";
-    }*/
-    var miniNav = document.querySelector("ytd-mini-guide-renderer");
-    if (miniNav) {
-        miniNav.innerHTML = "";
-    }
+  var miniNav = document.querySelector("ytd-mini-guide-renderer");
+  if (miniNav) {
+    miniNav.innerHTML = "";
+  }
 
-    // hide selected components, but keep some
-    var trendingMainNav = document.querySelector("#items > ytd-guide-entry-renderer:nth-child(2)");
-    if (trendingMainNav) {
-        trendingMainNav.innerHTML = "";
-    }
-    var subsMainNav = document.querySelector("#items > ytd-guide-entry-renderer:nth-child(3)");
-    if (subsMainNav) {
-        subsMainNav.innerHTML = "";
-    }
-    var laterNav = document.querySelector("#section-items > ytd-guide-entry-renderer:nth-child(2)");
-    if (laterNav) {
-        laterNav.innerHTML = "";
-    }
-    var purchasesNav = document.querySelector("#section-items > ytd-guide-entry-renderer:nth-child(3)");
-    if (purchasesNav) {
-        purchasesNav.innerHTML = "";
-    }
-    var likedNav = document.querySelector("#section-items > ytd-guide-entry-renderer:nth-child(4)");
-    if (likedNav) {
-        likedNav.innerHTML = "";
-    }
-    var collapsibleNav = document.querySelector("#section-items > ytd-guide-collapsible-entry-renderer");
-    if (collapsibleNav) {
-        collapsibleNav.innerHTML = "";
-    }
-    var subsNav = document.querySelector("#sections > ytd-guide-section-renderer:nth-child(2)");
-    if (subsNav) {
-        subsNav.innerHTML = "";
-    }
-    var moreNav = document.querySelector("#sections > ytd-guide-section-renderer:nth-child(3)");
-    if (moreNav) {
-        moreNav.innerHTML = "";
-    }
+  // hide selected components, but keep some
+  var trendingMainNav = document.querySelector(
+    "#items > ytd-guide-entry-renderer:nth-child(2)"
+  );
+  if (trendingMainNav) {
+    trendingMainNav.innerHTML = "";
+  }
+  var subsMainNav = document.querySelector(
+    "#items > ytd-guide-entry-renderer:nth-child(3)"
+  );
+  if (subsMainNav) {
+    subsMainNav.innerHTML = "";
+  }
+  var laterNav = document.querySelector(
+    "#section-items > ytd-guide-entry-renderer:nth-child(2)"
+  );
+  if (laterNav) {
+    laterNav.innerHTML = "";
+  }
+  var purchasesNav = document.querySelector(
+    "#section-items > ytd-guide-entry-renderer:nth-child(3)"
+  );
+  if (purchasesNav) {
+    purchasesNav.innerHTML = "";
+  }
+  var likedNav = document.querySelector(
+    "#section-items > ytd-guide-entry-renderer:nth-child(4)"
+  );
+  if (likedNav) {
+    likedNav.innerHTML = "";
+  }
+  var collapsibleNav = document.querySelector(
+    "#section-items > ytd-guide-collapsible-entry-renderer"
+  );
+  if (collapsibleNav) {
+    collapsibleNav.innerHTML = "";
+  }
+  var subsNav = document.querySelector(
+    "#sections > ytd-guide-section-renderer:nth-child(2)"
+  );
+  if (subsNav) {
+    subsNav.innerHTML = "";
+  }
+  var moreNav = document.querySelector(
+    "#sections > ytd-guide-section-renderer:nth-child(3)"
+  );
+  if (moreNav) {
+    moreNav.innerHTML = "";
+  }
 }
--- js/others.js
+++ js/others.js
@@ -1,10 +1,10 @@
-chrome.storage.sync.get(["hideTrending", "hideSubs"], function(result) {
-    var hideTrending = result.hideTrending;
-    var hideSubs = result.hideSubs;
-    if (hideTrending == true && window.location.href.includes("trending")) {
-        chrome.runtime.sendMessage({redirect: "https://www.youtube.com/"});
-    }
-    if (hideSubs == true && window.location.href.includes("subscriptions")) {
-        chrome.runtime.sendMessage({redirect: "https://www.youtube.com/"});
-    }
+chrome.storage.sync.get(["hideTrending", "hideSubs"], function (result) {
+  var hideTrending = result.hideTrending;
+  var hideSubs = result.hideSubs;
+  if (hideTrending == true && window.location.href.includes("trending")) {
+    chrome.runtime.sendMessage({ redirect: "https://www.youtube.com/" });
+  }
+  if (hideSubs == true && window.location.href.includes("subscriptions")) {
+    chrome.runtime.sendMessage({ redirect: "https://www.youtube.com/" });
+  }
 });
